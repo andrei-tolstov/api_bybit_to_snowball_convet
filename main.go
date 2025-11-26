521f4fcb06ead7f77dc0c2af7322c05a215b2d98
andrei <andrei.tolstov@gmail.com>
2025-11-26 20:27:17 +0300

    v0.1.1

--- main.go
+++ main.go
@@ -11,8 +11,34 @@ import (
 	// "github.com/mitchellh/mapstructure"
 )
 
+type DepositRecords struct {
+	Rows           []DepositRecord `json:"rows"`
+	NextPageCursor string          `json:"nextPageCursor"`
+}
 
+type DepositRecord struct {
+	Coin              string `json:"coin"`
+	Chain             string `json:"chain"`
+	Amount            string `json:"amount"`
+	TxID              string `json:"txID"`
+	Status            int    `json:"status"`
+	ToAddress         string `json:"toAddress"`
+	Tag               string `json:"tag"`
+	DepositFee        string `json:"depositFee"`
+	SuccessAt         string `json:"successAt"`
+	Confirmations     string `json:"confirmations"`
+	TxIndex           string `json:"txIndex"`
+	BlockHash         string `json:"blockHash"`
+	BatchReleaseLimit string `json:"batchReleaseLimit"`
+	DepositType       string `json:"depositType"`
+}
 
+const (
+    // Максимальный лимит записей на одну страницу API Bybit для депозита
+    DEPOSIT_DEFAULT_API_LIMIT = 50
+    // Интервал деления запросов на части в днях для депозита
+    DEPOSIT_DAYS_CHUNK_INTERVAL = 30
+)
 
 func main() {
 	BYBIT_API_KEY := os.Getenv("BYBIT_API_KEY")
@@ -26,7 +52,7 @@ func main() {
 		return
     }
 
-	// client := bybit.NewBybitHttpClient(BYBIT_API_KEY, BYBIT_API_SECRET, bybit.WithBaseURL(bybit.MAINNET))
+	client := bybit.NewBybitHttpClient(BYBIT_API_KEY, BYBIT_API_SECRET, bybit.WithBaseURL(bybit.MAINNET))
 	// максимальный срок хранения 2года
 	now := time.Now()
 	endDate := now
@@ -35,8 +61,7 @@ func main() {
 	// deposit
 	// https://bybit-exchange.github.io/docs/v5/asset/deposit/deposit-record
 	// 
-	// GetDepositRecords(client, startDate, endDate)
-	GetTimeSlice(startDate, endDate, 30)
+	GetDepositRecords(client, startDate, endDate)
 
 
 
@@ -56,15 +81,51 @@ func main() {
 
 // получаю записи о депозитах 
 func GetDepositRecords(client *bybit.Client, startDate time.Time, endDate time.Time) {
+	timeChunk := GetTimeSlice(startDate, endDate, DEPOSIT_DAYS_CHUNK_INTERVAL)
+	for timeChunkIndex := range timeChunk {
+		currentStart := timeChunk[timeChunkIndex]["start"]
+		currentEnd := timeChunk[timeChunkIndex]["end"]
 
-        params := map[string]interface{}{"limit": 50, "startTime": currentStart.UnixMilli(), "endTime": currentEnd.UnixMilli()}
-		serverResult, err := client.NewUtaBybitServiceWithParams(params).GetDepositRecords(context.Background())
-		if err != nil {
-			fmt.Println(err)
-			return
+		// Логирование в читаемом формате
+		startStr := time.UnixMilli(currentStart).Format(time.RFC3339)
+		endStr := time.UnixMilli(currentEnd).Format(time.RFC3339)
+		fmt.Printf("Запрос депозитов с %s по %s...\n", startStr, endStr)
+		
+		dataList := GetSliceDepositRecords(client, currentStart, currentEnd, "")
+		for dataSliceIndex := range dataList {
+			fmt.Println(dataList[dataSliceIndex])
 		}
-		fmt.Println(bybit.PrettyPrint(serverResult.Result))
-    
+	}
+}
+
+func GetSliceDepositRecords(client *bybit.Client, startTime int64, endTime int64, cursor string) []DepositRecord {
+	params := map[string]interface{}{"limit": DEPOSIT_DEFAULT_API_LIMIT, "startTime": startTime, "endTime": endTime, "cursor": cursor}
+	if cursor == "" {
+		delete(params, "cursor")
+	}
+	serverResult, err := client.NewUtaBybitServiceWithParams(params).GetDepositRecords(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return []DepositRecord{}
+	}
+	// fmt.Println(bybit.PrettyPrint(serverResult.Result))
+	var httpAnsver DepositRecords
+	tempBytes, _ := json.Marshal(serverResult.Result)
+	err = json.Unmarshal(tempBytes, &httpAnsver)
+	if err != nil {
+		// panic(err)
+		fmt.Printf("Ошибка декодирования JSON: %v\n", err)
+        return []DepositRecord{}
+	}
+	// fmt.Println(httpAnsver.List[0].Type)
+	rows := httpAnsver.Rows
+	if httpAnsver.NextPageCursor != "" {
+		iterRows := GetSliceDepositRecords(client, startTime, endTime, httpAnsver.NextPageCursor)
+		if len(iterRows) != 0 {
+			rows = append(rows, iterRows...)
+		}
+	}
+	return rows
 }
 
 func GetTimeSlice(startDate time.Time, endDate time.Time, days int) []map[string]int64 {
