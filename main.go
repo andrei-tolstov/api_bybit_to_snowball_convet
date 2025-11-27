6cd2800ca0a0d6757457fc6d1776be9742c71bec
andrei <andrei.tolstov@gmail.com>
2025-11-27 18:10:34 +0300

    deposit_90%

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"strconv"
 	bybit "github.com/andrei-tolstov/bybit.go.api.yield.history"
 	models "github.com/andrei-tolstov/bybit.go.api.yield.history/models"
 	// "github.com/mitchellh/mapstructure"
@@ -40,6 +41,31 @@ const (
     DEPOSIT_DAYS_CHUNK_INTERVAL = 30
 )
 
+// https://snowball-income.com/import-transactions
+type snowBallData struct {
+	Event           string
+	Date            string
+	Symbol          string
+	Price           string
+	Quantity        string
+	Currency        string
+	FeeTax          string
+	Exchange        string
+	NKD             string
+	FeeCurrency     string
+	DoNotAdjustCash string
+	Note            string
+}
+
+var snowBallDataSlice = []snowBallData{}
+
+// пока одно значение может в будущем будут еще
+var notDefaultNameCurrency = map[string]string{
+	"TON": "TON11419-USD",
+}
+
+var bufferWindowalFee = make(map[string]string)
+
 func main() {
 	BYBIT_API_KEY := os.Getenv("BYBIT_API_KEY")
     if BYBIT_API_KEY == "" {
@@ -62,6 +88,9 @@ func main() {
 	// https://bybit-exchange.github.io/docs/v5/asset/deposit/deposit-record
 	// 
 	GetDepositRecords(client, startDate, endDate)
+	for dataSliceIndex := range snowBallDataSlice {
+		fmt.Println(snowBallDataSlice[dataSliceIndex])
+	}
 
 
 
@@ -87,16 +116,91 @@ func GetDepositRecords(client *bybit.Client, startDate time.Time, endDate time.T
 		currentEnd := timeChunk[timeChunkIndex]["end"]
 
 		// Логирование в читаемом формате
-		startStr := time.UnixMilli(currentStart).Format(time.RFC3339)
-		endStr := time.UnixMilli(currentEnd).Format(time.RFC3339)
-		fmt.Printf("Запрос депозитов с %s по %s...\n", startStr, endStr)
+		// startStr := time.UnixMilli(currentStart).Format(time.RFC3339)
+		// endStr := time.UnixMilli(currentEnd).Format(time.RFC3339)
+		// fmt.Printf("Запрос депозитов с %s по %s...\n", startStr, endStr)
 		
 		dataList := GetSliceDepositRecords(client, currentStart, currentEnd, "")
 		for dataSliceIndex := range dataList {
-			fmt.Println(dataList[dataSliceIndex])
+			// fmt.Println(dataList[dataSliceIndex])
+			// all deposit = buy
+
+			newSnowBallData := snowBallData{
+				Event: "Buy",
+				Date: ConvertUnixString(dataList[dataSliceIndex].SuccessAt), //  конвертировать в формат 2020-02-01
+				Symbol: GetSymbol(dataList[dataSliceIndex].Coin), // конвертировать в COIN-USD
+				Price: "0", // запросить цену на время депозита
+				Quantity: dataList[dataSliceIndex].Amount, // кол-во
+				Currency: "USD", // всегда сводим к USD
+				FeeTax: GetWithdrawalFee(client, dataList[dataSliceIndex].Coin, dataList[dataSliceIndex].Chain), // рассчитать от комиссии за сеть??? использовать данные bybit (могут отличаться от использованых)
+				Exchange: "Bybit",
+				NKD: "0",
+				FeeCurrency: "",
+				DoNotAdjustCash: "True", // Не обновлять валюту
+				Note: "deposit",
+			}
+			snowBallDataSlice = append(snowBallDataSlice, newSnowBallData)
 		}
 	}
 }
+
+func GetWithdrawalFee(client *bybit.Client, coin string, chain string) string {
+	if value, ok := bufferWindowalFee[coin + "+" + chain]; ok {
+		// fmt.Println("использую кэш")
+        return value
+    }
+    params := map[string]interface{}{"coin": coin}
+    serverResult, err := client.NewUtaBybitServiceWithParams(params).GetCoinInfo(context.Background())
+    if err != nil {
+		fmt.Errorf("ошибка GetWithdrawalFee: %w", err)
+        return "0"
+    }
+    var apiResponse models.CoinInfoResult
+    tempBytes, err := json.Marshal(serverResult.Result)
+    if err != nil {
+		fmt.Errorf("ошибка GetWithdrawalFee: %w", err)
+        return "0"
+    }
+    
+    if err := json.Unmarshal(tempBytes, &apiResponse); err != nil {
+		fmt.Errorf("ошибка GetWithdrawalFee: %w", err)
+        return "0"   
+	}
+	
+    for index := range apiResponse.Rows[0].Chains {
+        // Комиссия фиксирована для каждой пары (Монета + Сеть)
+        if apiResponse.Rows[0].Chains[index].Chain == chain {
+			bufferWindowalFee[coin + "+" + chain] = apiResponse.Rows[0].Chains[index].WithdrawFee
+            return bufferWindowalFee[coin + "+" + chain]
+        }
+    }
+
+    //Если пара не найдена
+	bufferWindowalFee[coin + "+" + chain] = "0"
+    return "0"
+}
+
+
+func GetSymbol(symbol string) string {
+    if value, ok := notDefaultNameCurrency[symbol]; ok {
+        return value
+    }
+    return symbol + "-USD"
+}
+
+func ConvertUnixString(unixString string) string {
+	// Преобразование строки в целое число
+	unixMilli, err := strconv.ParseInt(unixString, 10, 64)
+	if err != nil {
+		fmt.Errorf("ошибка при парсинге строки UnixTime: %w", err)
+		return ""
+	}
+    // Преобразование миллисекунд в тип time.Time
+    t := time.UnixMilli(unixMilli)
+    formattedDate := t.Format("2006-01-02")
+	return formattedDate
+}
+
 
 func GetSliceDepositRecords(client *bybit.Client, startTime int64, endTime int64, cursor string) []DepositRecord {
 	params := map[string]interface{}{"limit": DEPOSIT_DEFAULT_API_LIMIT, "startTime": startTime, "endTime": endTime, "cursor": cursor}
@@ -113,7 +217,6 @@ func GetSliceDepositRecords(client *bybit.Client, startTime int64, endTime int64
 	tempBytes, _ := json.Marshal(serverResult.Result)
 	err = json.Unmarshal(tempBytes, &httpAnsver)
 	if err != nil {
-		// panic(err)
 		fmt.Printf("Ошибка декодирования JSON: %v\n", err)
         return []DepositRecord{}
 	}
@@ -145,9 +248,6 @@ func GetTimeSlice(startDate time.Time, endDate time.Time, days int) []map[string
 	return timeChunk
 }
 
-
-
-
 func GetTransaction(BYBIT_API_KEY, BYBIT_API_SECRET string) {
 	client := bybit.NewBybitHttpClient(BYBIT_API_KEY, BYBIT_API_SECRET, bybit.WithBaseURL(bybit.MAINNET))
 	params := map[string]interface{}{"accountType": "UNIFIED", "startTime": 1762290000000, "endTime": 1762549200000}
@@ -176,8 +276,6 @@ func GetAccountWallet(BYBIT_API_KEY, BYBIT_API_SECRET string) {
 	}
 	fmt.Println(bybit.PrettyPrint(accountResult))
 }
-
-
 
 func GetEarnRedeemOrder(BYBIT_API_KEY, BYBIT_API_SECRET string) {
 	client := bybit.NewBybitHttpClient(BYBIT_API_KEY, BYBIT_API_SECRET, bybit.WithBaseURL(bybit.MAINNET))
